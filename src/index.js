e9cf6eb20ddb34f37c14e0c41c9602a61682c9b4
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-26 16:42:05 +0200

    add complete project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyCXZpPj7u-8Rxzp33efs9CLpZrTXP2X4YY",
+  authDomain: "chat-react-bab57.firebaseapp.com",
+  projectId: "chat-react-bab57",
+  storageBucket: "chat-react-bab57.appspot.com",
+  messagingSenderId: "691219591348",
+  appId: "1:691219591348:web:536b2ade67d1df6f1927e1",
+});
+
+export const Context = React.createContext(null);
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Context.Provider value={{ firebase, auth, firestore }}>
+      <App />
+    </Context.Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
